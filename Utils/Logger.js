30bbd936eb4521783284015afc370db38aaeea04
RealBytes86 <echtedeveloper@gmail.com>
2024-02-25 17:52:31 +0100

    add Logger

---
+++ Utils/Logger.js
@@ -0,0 +1,32 @@
+const winston = require('winston');
+const chalk = require("chalk")
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss'}), winston.format.printf(({ level, message, timestamp }) => {
+        switch(level) {
+            case "info":
+                return `${chalk.cyan(`[${timestamp}]`)} [Server worker/${level.toUpperCase()}]: ${message}`;
+            case "error":
+                return `${chalk.cyan(`[${timestamp}]`)} ${chalk.red(`[Server worker/${level.toUpperCase()}]: ${message}`)}`;
+            case "warn":
+                return `${chalk.cyan(`[${timestamp}]`)} ${chalk.yellow(`[Server worker/${level.toUpperCase()}]: ${message}`)}`;
+            case "debug":
+                return `${chalk.cyan(`[${timestamp}]`)} ${chalk.blue(`[Server worker/${level.toUpperCase()}]: ${message}`)}`;
+            case "critical":
+                return `${chalk.cyan(`[${timestamp}]`)} ${chalk.redBright(`[Server worker/${level.toUpperCase()}]: ${message}`)}`;
+            default:
+                return `${chalk.cyan(`[${timestamp}]`)} [Server worker/${level.toUpperCase()}]: ${message}`;
+        }
+    })
+  ),
+  transports: [
+    new winston.transports.Console(),
+  ],
+});
+
+module.exports = { 
+    getLogger: () => {
+        return logger;
+    }
+}
